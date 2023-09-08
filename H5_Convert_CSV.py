891b2140f9f811ff6b61386e5ebec6ea430221dc
Sunny You <sunny.jiang.you@gmail.com>
2023-09-08 13:49:11 -0400

    Attempting to make H5 to CSV conversion script automatic.

--- H5_Convert_CSV.py
+++ H5_Convert_CSV.py
@@ -2,27 +2,29 @@ import h5py
 import os
 import numpy as np
 import pandas as pd
+import sys
+
+filename, gis_id, category, latitude, longitude = sys.argv[:5]
+date, time = filename[23:30], filename[32:34]
+
 os.chdir('H5')
-f = h5py.File('3B-HHR-E.MS.MRG.3IMERG.20230907-S173000-E175959.1050.V06D.RT-H5', 'r')
+f = h5py.File(filename, 'r')
 grid = f['Grid']
+
 longitude_values = np.repeat(list(grid['lon']), 1800)
 latitude_values = list(grid['lat'])*3600
 precipitation_values = np.array(list(grid['precipitationCal'])).flatten()
+
 dataset = pd.DataFrame({"lon": longitude_values, "lat": latitude_values, "precipitation": precipitation_values})
 dataset.columns = [grid['lon'].attrs['standard_name'].decode() + " (" + grid['lon'].attrs['units'].decode() + ")",
                    grid['lat'].attrs['standard_name'].decode() + " (" + grid['lat'].attrs['units'].decode() + ")",
                    "Precipitation (" + grid['precipitationCal'].attrs['units'].decode() + ")",]
-dataset.head()
-np.amax(dataset['latitude (degrees_north)'])
-dataset.to_csv('IMERG_20230907_1800Z.csv', index=False)
-dataset = dataset[dataset['longitude (degrees_east)'] <= -44.7]
-dataset = dataset[dataset['longitude (degrees_east)'] >= -56.8]
-dataset = dataset[dataset['latitude (degrees_north)'] <= 22.7]
-dataset = dataset[dataset['latitude (degrees_north)'] >= 10.6]
-print(dataset.shape)
-dataset.head()
+
+dataset = dataset[dataset['longitude (degrees_east)'] <= (longitude + 6.0)]
+dataset = dataset[dataset['longitude (degrees_east)'] >= (longitude - 6.1)]
+dataset = dataset[dataset['latitude (degrees_north)'] <= (latitude + 6.1)]
+dataset = dataset[dataset['latitude (degrees_north)'] >= (latitude - 6.0)]
+
 image = dataset.pivot(index='latitude (degrees_north)', columns='longitude (degrees_east)', values='Precipitation (mm/hr)')
 image = image.T
-image = image.iloc[:, :]
-image.head()
-image.to_csv('ATL_202313_C3_2023090718.csv', index=False, header = False)
+image.to_csv(f'{gis_id}_{category}_{date}{time}.csv', index=False, header = False)
