d77e2603878ad7cb4e00ce0a1edeb1c8a39596a0
Sunny You <sunny.jiang.you@gmail.com>
2023-11-20 11:03:41 -0500

    Created notebook to collect SHIPS data.

--- H5_Convert_CSV.py
+++ H5_Convert_CSV.py
@@ -2,32 +2,43 @@ import h5py
 import os
 import numpy as np
 import pandas as pd
-import sys
+import pathlib
 
-filename, basin, ident, category, latitude, longitude = sys.argv[1:7]
-date, time = filename[23:31], filename[33:35]
-year = date[0:4]
-latitude, longitude = float(latitude), float(longitude)
-filename, basin, ident, category, date, time = str(filename), str(basin), str(ident), str(category), str(date), str(time)
+os.chdir('Data/H5')
 
-os.chdir('H5')
-f = h5py.File(filename, 'r')
-grid = f['Grid']
+def main():
+    for file in list(pathlib.Path('H5').glob('*.RT-H5')):
+        h5_convert_csv(file[3:], ???, ???, ???, ???, ???)
+        
 
-longitude_values = np.repeat(list(grid['lon']), 1800)
-latitude_values = list(grid['lat'])*3600
-precipitation_values = np.array(list(grid['precipitationCal'])).flatten()
+def h5_convert_csv(filename, basin, ident, category, latitude, longitude):
+    date, time = filename[23:31], filename[33:35]
+    year = date[0:4]
+    latitude, longitude = float(latitude), float(longitude)
+    filename, basin, ident, category, date, time = str(filename), str(basin), str(ident), str(category), str(date), str(time)
 
-dataset = pd.DataFrame({"lon": longitude_values, "lat": latitude_values, "precipitation": precipitation_values})
-dataset.columns = [grid['lon'].attrs['standard_name'].decode() + " (" + grid['lon'].attrs['units'].decode() + ")",
-                   grid['lat'].attrs['standard_name'].decode() + " (" + grid['lat'].attrs['units'].decode() + ")",
-                   "Precipitation (" + grid['precipitationCal'].attrs['units'].decode() + ")",]
+    f = h5py.File(filename, 'r')
+    grid = f['Grid']
 
-dataset = dataset[dataset['longitude (degrees_east)'] <= (longitude + 6.0)]
-dataset = dataset[dataset['longitude (degrees_east)'] >= (longitude - 6.1)]
-dataset = dataset[dataset['latitude (degrees_north)'] <= (latitude + 6.1)]
-dataset = dataset[dataset['latitude (degrees_north)'] >= (latitude - 6.0)]
+    longitude_values = np.repeat(list(grid['lon']), 1800)
+    latitude_values = list(grid['lat'])*3600
+    precipitation_values = np.array(list(grid['precipitationCal'])).flatten()
 
-image = dataset.pivot(index='latitude (degrees_north)', columns='longitude (degrees_east)', values='Precipitation (mm/hr)')
-image = image.T
-image.to_csv(f'{basin}_{year}{ident}_{category}_{date}{time}.csv', index=False, header = False)
+    dataset = pd.DataFrame({"lon": longitude_values, "lat": latitude_values, "precipitation": precipitation_values})
+    dataset.columns = [grid['lon'].attrs['standard_name'].decode() + " (" + grid['lon'].attrs['units'].decode() + ")",
+                    grid['lat'].attrs['standard_name'].decode() + " (" + grid['lat'].attrs['units'].decode() + ")",
+                    "Precipitation (" + grid['precipitationCal'].attrs['units'].decode() + ")",]
+
+    dataset = dataset[dataset['longitude (degrees_east)'] <= (longitude + 6.0)]
+    dataset = dataset[dataset['longitude (degrees_east)'] >= (longitude - 6.1)]
+    dataset = dataset[dataset['latitude (degrees_north)'] <= (latitude + 6.1)]
+    dataset = dataset[dataset['latitude (degrees_north)'] >= (latitude - 6.0)]
+
+    image = dataset.pivot(index='latitude (degrees_north)', columns='longitude (degrees_east)', values='Precipitation (mm/hr)')
+    image = image.T
+    os.chdir('../../IMERG_CSV')
+    image.to_csv(f'{basin}_{year}{ident}_{category}_{date}{time}.csv', index=False, header = False)
+    os.chdir('../Data/H5')
+
+if __name__ == '__main__':
+    main()
